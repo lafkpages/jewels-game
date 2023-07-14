5d2a895796d2c66a21bf0e8ad3fdf7d153b7a8a1
lafkpages <soy.lafk+gh@gmail.com>
2023-07-15 01:02:12 +0900

    Basic game board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
+<script lang="ts">
+  const board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+</script>
+
 <h1>Jewels Game</h1>
+
+<table>
+  {#each board as row}
+    <tr>
+      {#each row as cell}
+        <td>{cell}</td>
+      {/each}
+    </tr>
+  {/each}
+</table>
