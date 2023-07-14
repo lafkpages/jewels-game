b9b21abc7ea1e8b479ae264136ba488f4d3860d6
lafkpages <soy.lafk+gh@gmail.com>
2023-07-15 02:59:53 +0900

    Unfinished stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,73 @@
     [4, 5, 6],
     [7, 8, 9],
   ];
+
+  function onJewelDragStart(e: DragEvent) {
+    if (!e.dataTransfer) {
+      return;
+    }
+
+    if (!(e.target instanceof HTMLElement)) {
+      return;
+    }
+
+    const row = e.target.dataset.row;
+    const cell = e.target.dataset.cell;
+
+    if (row === undefined || cell === undefined) {
+      return;
+    }
+
+    // Drag and drop data
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/plain;p=row", row);
+    e.dataTransfer.setData("text/plain;p=cell", cell);
+  }
+
+  function onJewelDragOver(e: DragEvent) {
+    if (!e.dataTransfer) {
+      return;
+    }
+
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+  }
+
+  function onJewelDrop(e: DragEvent) {
+    if (!e.dataTransfer) {
+      return;
+    }
+
+    const row = parseInt(e.dataTransfer.getData("text/plain;p=row"));
+    const cell = parseInt(e.dataTransfer.getData("text/plain;p=cell"));
+
+    if (isNaN(row) || isNaN(cell)) {
+      return;
+    }
+
+    console.log(row, cell, e.target);
+  }
 </script>
 
 <h1>Jewels Game</h1>
 
 <table>
-  {#each board as row}
+  {#each board as row, rowIndex}
     <tr>
-      {#each row as cell}
+      {#each row as cell, cellIndex}
         <td>
-          <div class="jewel" style:--jewel={3} draggable="true" />
+          <div
+            class="jewel"
+            style:--jewel={rowIndex * 3 + cellIndex}
+            draggable="true"
+            on:dragstart={onJewelDragStart}
+            on:dragover={onJewelDragOver}
+            on:drop={onJewelDrop}
+            role="cell"
+            tabindex="0"
+            data-row={rowIndex}
+            data-cell={cellIndex}
+          />
         </td>
       {/each}
     </tr>
