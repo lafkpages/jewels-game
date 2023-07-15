36260f498b12d62f6e8e55db94a5aa187c572df1
lafkpages <soy.lafk+gh@gmail.com>
2023-07-16 01:37:55 +0900

    Triple board size

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import { generateBoardEnsureNoMatches } from "$lib";
 
 import type { PageServerLoad } from "./$types";
 
-const boardSize = 5;
+const boardSize = 15;
 
 export const load: PageServerLoad = () => {
   const board = generateBoardEnsureNoMatches([boardSize]);
