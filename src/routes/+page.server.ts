32ec6c47659e4ab07bae9c62e18d7c801f771cc5
lafkpages <soy.lafk+gh@gmail.com>
2023-07-16 01:47:50 +0900

    Increase board generation loop limit

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,7 +5,7 @@ import type { PageServerLoad } from "./$types";
 const boardSize = 15;
 
 export const load: PageServerLoad = () => {
-  const board = generateBoardEnsureNoMatches([boardSize]);
+  const board = generateBoardEnsureNoMatches([boardSize], 50000);
   return {
     board,
   };
