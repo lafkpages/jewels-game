a1ed29251fbd2f6bcbcaa3400108cc2c586dbe14
lafkpages <soy.lafk+gh@gmail.com>
2023-08-27 19:01:41 +0200

    capitalize and kebabCaseToHuman lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,11 @@ export * from "$lib/types";
 export function sum(a: number[]) {
   return a.reduce((a, b) => a + b, 0);
 }
+
+export function kebabCaseToHuman(s: string) {
+  return capitalize(s.replace(/-/g, " "));
+}
+
+export function capitalize(s: string) {
+  return s[0].toUpperCase() + s.slice(1);
+}
