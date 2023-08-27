12d537edb1e4c55981ef054f8e922d1d02674cd5
lafkpages <soy.lafk+gh@gmail.com>
2023-08-27 20:25:27 +0200

    Nav active indicator animation

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -5,7 +5,11 @@
 
   export let sections: string[];
 
+  let activeElm: HTMLSpanElement | null = null;
+
   $: activeSection = sections.indexOf($page.url.hash.slice(1));
+
+  let activeSectionHeight: number = 0;
   $: activeSectionHeight = new Array(Math.max(activeSection, 0))
     .fill(0)
     .reduce(
@@ -15,19 +19,43 @@
         (document.getElementById(`${sections[i]}-nav-link`)?.clientHeight || 0),
       0
     );
+  $: {
+    if (activeElm) {
+      console.debug(activeSectionHeightOld, activeSectionHeight);
+      activeElm.animate(
+        [
+          {
+            top: `${activeSectionHeightOld}px`,
+          },
+          {
+            top: `${activeSectionHeight}px`,
+          },
+        ],
+        {
+          duration: 100,
+          easing: "ease-in-out",
+        }
+      );
+      activeSectionHeightOld = activeSectionHeight;
+    }
+  }
+
+  let activeSectionHeightOld = activeSectionHeight;
 </script>
 
 <nav>
-  {#key $page.url.hash}
-    <span class="active" style:--pos="{activeSectionHeight}px">-</span>
-    <ul>
-      {#each sections as section}
-        <li id="{section}-nav-link">
-          <a href="#{section}">{kebabCaseToHuman(section)}</a>
-        </li>
-      {/each}
-    </ul>
-  {/key}
+  <span
+    class="active"
+    style:--pos="{activeSectionHeight}px"
+    bind:this={activeElm}>-</span
+  >
+  <ul>
+    {#each sections as section}
+      <li id="{section}-nav-link">
+        <a href="#{section}">{kebabCaseToHuman(section)}</a>
+      </li>
+    {/each}
+  </ul>
 </nav>
 
 <style>
