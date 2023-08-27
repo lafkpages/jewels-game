665c6ebb5dd200af64a5f20ab29fe856eddce723
lafkpages <soy.lafk+gh@gmail.com>
2023-08-27 11:51:49 +0200

    Custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<main>
+  <h1>{$page.status}: {$page.error?.message}</h1>
+</main>
+
+<style>
+  main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
